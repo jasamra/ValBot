9069d7b56cd4b34ee08ef51fcc51284e31f1af95
Jasprit Samra <jasisamr@ucsc.edu>
2020-07-17 00:38:50 -0700

    3rd

--- bot.py
+++ bot.py
@@ -2,11 +2,31 @@ import discord
 from discord.ext import commands, tasks
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+my_url = 'https://playvalorant.com/en-us/news/'
+#opening connection/grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
 
 
+#html parsing
+page_soup = soup(page_html, "html.parser")
+#grabs
+containers = page_soup.findAll("div",{"class":"NewsArchive-module--newsCardWrapper--2OQiG"})
 
-URL = 'https://playvalorant.com/en-us/news/'
+links = page_soup.findAll('a',{"href" : True})
+	
+
+
 client = commands.Bot(command_prefix = '.')
+
+def getDate():
+	storedDate = []
+	for container in containers:
+		
+		storedDate.append(container.find('p',{"class" : "NewsCard-module--published--37jmR"}).text.strip())
+
+	return storedDate
 
 @client.event
 async def on_ready():
@@ -14,8 +34,24 @@ async def on_ready():
 
 
 @task.loop(seconds = 60)
-async def function():
-	pass
+async def on_ready():	
+	container = containers[0]
+	title = container.find('h5',{"class" : "heading-05 NewsCard-module--title--1MoLu"}).text.strip()
+	description = container.find('p',{"class" : "copy-02 NewsCard-module--description--3sFiD"}).text.strip()
+	date = container.find('p',{"class" : "NewsCard-module--published--37jmR"}).text.strip()
+	link = container.find('a', {"href" : True})
+
+	if len(title) > 0:
+		
+		print(title)
+		print(f'	{date}')
+		print(f'	{description}\n')	
+		if 'href' in link.attrs:		
+			print(f"https://playvalorant.com{str(link.attrs['href'])} \n")
+
+	getDate()
+
+
 
 
 client.run('NzMzMTg4MjUzNTIzNjQwMzMx.Xw_ogg.mhTZ5JPsjfP16ds48f-CTqfYt-M')
--- web.py
+++ web.py
@@ -14,21 +14,34 @@ page_soup = soup(page_html, "html.parser")
 containers = page_soup.findAll("div",{"class":"NewsArchive-module--newsCardWrapper--2OQiG"})
 
 links = page_soup.findAll('a',{"href" : True})
-
+	
 #for container in containers:
-container = containers[0]
-title = container.find('h5',{"class" : "heading-05 NewsCard-module--title--1MoLu"}).text.strip()
-print(title)
-description = container.find('p',{"class" : "copy-02 NewsCard-module--description--3sFiD"}).text.strip()
-date = container.find('p',{"class" : "NewsCard-module--published--37jmR"}).text.strip()
-link = container.find('a', {"href" : True})
 
-if len(title) > 0 :
-	print(f'	{date}')
-	print(f'	{description}\n')	
-	if 'href' in link.attrs:		
-		print(f"https://playvalorant.com{str(link.attrs['href'])} \n")
+def getDate():
+	storedDate = []
+	for container in containers:
+		
+		storedDate.append(container.find('p',{"class" : "NewsCard-module--published--37jmR"}).text.strip())
 
+	return storedDate
 
+def scrape():		
+	container = containers[0]
+	title = container.find('h5',{"class" : "heading-05 NewsCard-module--title--1MoLu"}).text.strip()
+	description = container.find('p',{"class" : "copy-02 NewsCard-module--description--3sFiD"}).text.strip()
+	date = container.find('p',{"class" : "NewsCard-module--published--37jmR"}).text.strip()
+	link = container.find('a', {"href" : True})
 
+	if len(title) > 0:
+		
+		print(title)
+		print(f'	{date}')
+		print(f'	{description}\n')	
+		if 'href' in link.attrs:		
+			print(f"https://playvalorant.com{str(link.attrs['href'])} \n")
+
+	getDate()
+
+	
+scrape()
 
